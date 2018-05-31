cc87f968b6f4a496812a9a8b165caec13894eeec
Brendan <brendan@tangibleinteraction.com>
2018-05-31 00:30:11 -0700

    added sACN to LEDs (as a single pixel) + cleanup

--- SPORE_FW/SPORE_FW.ino
+++ SPORE_FW/SPORE_FW.ino
@@ -1,29 +1,29 @@
 /*
- *  SPORE FIRMWARE
- *  Copyright (C) 2018 PWRFL
- *  
- *  @authors Leó Stefánsson, Tim Rolls, and Brendan Matkin
- *
- *  This sketch contains the firmware for the "Spore" hardware.
- *  The intent is to create an installation that periodically remaps wireless LEDs ("Spores")
- *  and fascilitates an interactive element.
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    SPORE FIRMWARE
+    Copyright (C) 2018 PWRFL
+
+    @authors Leó Stefánsson, Tim Rolls, and Brendan Matkin
+
+    This sketch contains the firmware for the "Spore" hardware.
+    The intent is to create an installation that periodically remaps wireless LEDs ("Spores")
+    and fascilitates an interactive element.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 extern "C" {
-  #include "user_interface.h"           // allows wifi_set_sleep_type
+#include "user_interface.h"           // allows wifi_set_sleep_type
 }
 #include "settings.h"                   // local settings!
 //#include "FS.h"                       // File System / SPIFFS
@@ -37,7 +37,7 @@ extern "C" {
 
 const uint16_t PixelCount = 16;                                   // DO NOT CHANGE!!
 NeoGamma<NeoGammaTableMethod> colorGamma;                         // for any fade animations, best to correct gamma
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, 3);// pin 3 is DMA anyway so the value is actuallly ignored..
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> pixels(PixelCount);   // pin 3 is DMA anyway so the value is actuallly ignored..
 
 WiFiManager wifiManager;
 E131 e131;
@@ -58,7 +58,8 @@ void setup() {
     Serial.println("no one connected and I timed out. I'll try to connect (in STA) again!"); delay(3000);
     ESP.reset(); delay(5000);
   }
-  address = WiFi.localIP()[3];                // get address
+  //address = WiFi.localIP()[3];              // get address
+  address = 1;                                // TESTING - just set to 1 manually for now
   deviceName.concat(address);
 
   /* init LED PIN */
@@ -67,15 +68,15 @@ void setup() {
 
   /* LEDs */
   delay(100);
-  strip.Begin();
-  strip.Show();
+  pixels.Begin();
+  pixels.Show();
 
-  /* sACN */
-  /* The following method creates 
-   * the UDP port and joins the 
-   * multicast group for you.
-   */
-  e131.begin(E131_MULTICAST, 1, 1);           // (E131_UNICAST/E131_MULTICAST, universeNumber,number of universes) 
+  /* sACN
+     The following method creates
+     the UDP port and joins the
+     multicast group for you.
+  */
+  e131.begin(E131_MULTICAST, 1, 1);           // (E131_UNICAST/E131_MULTICAST, universeNumber,number of universes)
 
   /* who am I this time?  */
   delay(100);
@@ -92,13 +93,22 @@ void setup() {
 
 
 void loop() {
-  uint16_t numChannels = e131.parsePacket();
-  if (numChannels > 0) {
-    Serial.printf("Universe %u / %u Channels | Packet#: %u / Errors: %u / CH1: %u\n",
-                e131.universe,              // The Universe for this packet
-                numChannels,                // Number of channels in this packet
-                e131.stats.num_packets,     // Packet counter
-                e131.stats.packet_errors,   // Packet error counter
-                e131.data[0]);              // Dimmer data for Channel 1
+  if (e131.parsePacket()) {
+    uint8_t r = e131.data[address];
+    uint8_t g = e131.data[address+1];
+    uint8_t b = e131.data[address+2];
+    RgbColor col = colorGamma.Correct(RgbColor(r, g, b));
+    for (int i = 0; i < PixelCount; i++) {
+      pixels.SetPixelColor(i, col);
     }
+  }
+//  uint16_t numChannels = e131.parsePacket();
+//  if (numChannels > 0) {
+//    Serial.printf("Universe %u / %u Channels | Packet#: %u / Errors: %u / CH1: %u\n",
+//                  e131.universe,              // The Universe for this packet
+//                  numChannels,                // Number of channels in this packet
+//                  e131.stats.num_packets,     // Packet counter
+//                  e131.stats.packet_errors,   // Packet error counter
+//                  e131.data[0]);              // Dimmer data for Channel 1
+//  }
 }
--- SPORE_FW/settings.h
+++ SPORE_FW/settings.h
@@ -9,22 +9,24 @@
 
 /* -- network settings -- */
 const char* AP_SSID  = "SPORE-TEST";
-const char* ssid     = WIFI_SSID;             // ssid from wifi_config.h
-const char* password = WIFI_PASSWORD;         // password from wifi_config.h
-String deviceName    = "Spore_";              // used for DHCP
+const char* ssid     = WIFI_SSID;        // ssid from wifi_config.h
+const char* password = WIFI_PASSWORD;    // password from wifi_config.h
+String deviceName    = "Spore_";         // used for DHCP
 
 
 
 /* -- firmware settings -- */
 #define HW_VERSION "0.0.3"
 #define HW_PHASE   "-beta"
-const int FW_VERSION = 100;                   // 0.1.00, convention: 1.2.10 = 1210, 0.5.9 = 509, no leading 0s or it is interpreted as octal.. learned that the hard way!
+const int FW_VERSION = 100;              // 0.1.00, convention: 1.2.10 = 1210, 0.5.9 = 509, no leading 0s or it is interpreted as octal.. learned that the hard way!
 #define FW_PHASE   "-alpha"
-float fwCheckButtonTime = 2000.0f;            // how long to hold button down.
-//const char* fwUrlBase = "http://10.0.3.100/";
-//const char* fwName = "spore_fw";
+//float fwCheckButtonTime = 2000.0f;     // how long to hold button down.
 
+
+/* -- general settings -- */
+#define CHAN_PER_FIXTURE 3               // number of channels per fixture
 uint8_t address = 99;
+
 
 
 
