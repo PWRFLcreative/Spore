8bea79aeb0b68a092ea9bea6964d2668f718f3ca
Brendan <brendan@tangibleinteraction.com>
2018-08-02 19:14:37 -0700

    initial server commit (simple working - version used for creative tech Vancouver presentation)

---
+++ SPORE_SERVER/js/index.js
@@ -0,0 +1,48 @@
+const {ipcRenderer} = require('electron')
+
+let status_console = document.getElementById('status-console')
+let button_fw      = document.getElementById('checkFirmwareButton')
+let button_scan    = document.getElementById('scanButton')
+
+
+
+window.onload = () => {
+  statusConsole("chillin'..")
+}
+
+
+
+function clearStatusConsole() {
+  status_console.innerHTML = "&nbsp;"
+}
+
+function statusConsole(msg) {
+  status_console.innerHTML += msg + "<br>"
+  status_console.scrollTop = status_console.scrollHeight
+}
+
+function checkFirmware() {
+  ipcRenderer.send('checkFirmware')
+  statusConsole("devices checking for new firmware..")
+}
+
+function scanDevices() {
+  ipcRenderer.send('scanDevices')
+  statusConsole("scanning devices (and configure addresses)..")
+}
+
+
+
+/* -------- MESSAGING (main <--> render processes) -------- */
+ipcRenderer.on('clear-status-console', (event) => {
+  clearStatusConsole()
+})
+ipcRenderer.on('devices-connected', (event, arg) => {
+  statusConsole(arg + " devices connected.")
+})
+
+
+
+/* ------ EVENT LISTENERS ------ */
+button_fw.addEventListener('click', checkFirmware)    // maybe try dblclick!
+button_scan.addEventListener('click', scanDevices)
